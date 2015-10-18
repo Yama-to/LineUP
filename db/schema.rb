9b29807211ca8c7f81e2be645daabf0143d36454
Yamato <wonka.bm@gmail.com>
2015-10-18 09:14:43 -0700

    fix que mode

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018055736) do
+ActiveRecord::Schema.define(version: 20151018161300) do
+
+  create_table "ques", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "store_id",   limit: 4
+    t.integer  "level",      limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "ques", ["store_id"], name: "index_ques_on_store_id", using: :btree
+  add_index "ques", ["user_id"], name: "index_ques_on_user_id", using: :btree
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   limit: 255, default: "", null: false
@@ -34,4 +51,6 @@ ActiveRecord::Schema.define(version: 20151018055736) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "ques", "stores"
+  add_foreign_key "ques", "users"
 end
