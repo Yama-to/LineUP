9c7c209d695d540f73361a84745d1ac3764556c2
Yamato <wonka.bm@gmail.com>
2015-10-18 13:53:14 -0700

    design progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # redirect
+  before_action :authenticate_user!
+
   # devise parameters
   before_action :configure_permitted_parameters, if: :devise_controller?
 
